b001bec6ed0091822c2fdf3ed115c1857f2c1a88
denzil99 <19denis99@mail.ru>
2023-04-06 22:38:33 +0500

    доделал прогу

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,15 +4,41 @@ namespace ConsoleApp3
 {
     internal class Program
     {
+        // READ!!!!
+        // Я использовал byte вместо uint т.к
+
+
         static void Main(string[] args)
         {
-            //bool iscorrect;
-            //byte age;
-            //iscorrect = byte.TryParse(Console.ReadLine(), out age);
-            //Console.WriteLine(age);
-            //Console.ReadKey
+            var person = GetInfo();
+            PrintPerson(person);
+            Console.ReadKey();
         }
 
+        static void PrintPerson((string name, string surname, byte age,
+                string[] pets, string[] favColors) tuple)
+        {
+
+            Console.WriteLine($"Name: {tuple.name} ");
+            Console.WriteLine($"Surname: {tuple.surname} ");
+            Console.WriteLine($"Age: {tuple.age} ");
+
+            if (tuple.pets != null)
+            {
+                int i = 0;
+                foreach (var pet in tuple.pets)
+                {
+                    Console.WriteLine($"Name of {++i} pet is {pet}");
+                }
+            }
+            else { Console.WriteLine($"{tuple.name} don't have a pet"); }
+
+
+            for (int i = 0; i < tuple.favColors.Length; i++)
+            {
+                Console.WriteLine($"Your {i + 1} favourite color is: {tuple.favColors[i]}");
+            }
+        }
 
 
         /// <summary>
@@ -20,64 +46,136 @@ namespace ConsoleApp3
         /// </summary>
         /// <returns></returns>
 
-        public (string, string) GetInfo()
+        static (string, string, byte, string[], string[]) GetInfo()
         {
-            string name;
-            string surname;
-            bool iscorrect;
-            byte age;
-            bool havePet;
-            byte numPets;
 
-            string[] pets;
+
+            byte numPets;
+            byte numColor;
+
+
+            (string name, string surname, byte age,
+                string[] pets, string[] favColors) Person;
 
 
             Console.WriteLine("Enter yor name: ");
-            name = Console.ReadLine();
+            Person.name = ChekedString();
+
             Console.WriteLine("Enter yor surname: ");
-            surname = Console.ReadLine();
+            Person.surname = ChekedString();
+
             Console.Write("Enter your age: ");
-            age = CheckedNum();
+            Person.age = CheckedNum();
+
             Console.WriteLine("Do u have a pet? Y/N");
-            string tempAns = Console.ReadLine();
-            if (tempAns == "Y")
+            string tempAns = ChekedString();
+
+            Person.pets = new string[0];
+
+            if (tempAns.ToLower() == "y")
             {
-                havePet = true;
-                Console.WriteLine("How many pets do u have?: ");
+                //havePet = true;
+                Console.WriteLine("How many pets do u have? ");
                 numPets = CheckedNum();
-                pets = Pets(numPets);
+                Person.pets = Pets(numPets);
             }
-            else { havePet = false; }
+            else if (tempAns.ToLower() == "n")
+            {
 
+                ////havePet = false;
+                Person.pets = new string[0];
+                //Person.pets[0] = "Don't have a pet";
+            }
 
+            Console.WriteLine("How many colors do u like?");
+            numColor = CheckedNum();
+            Person.favColors = FavColors(numColor);
+
+            return (Person.name, Person.surname, Person.age, Person.pets, Person.favColors);
 
         }
 
-        public string[] Pets(byte numPets)
+        static string[] Pets(byte numPets)
         {
             string[] petNames = new string[numPets];
+
             Console.WriteLine("Enter your pets names");
+
             for (int i = 0; i < numPets; i++)
             {
                 Console.WriteLine($"Name of {i + 1} pet is: ");
-                string tempPetName = Console.ReadLine();
+                string tempPetName = ChekedString();
                 petNames[i] = tempPetName;
             }
-            return petNames;
 
+            return petNames;
         }
 
-        public byte CheckedNum()
+        static string[] FavColors(byte numFavColors)
+        {
+            string[] tempFavColors = new string[numFavColors];
+            Console.WriteLine("Enter your favourite colors:");
+            for (int i = 0; i < numFavColors; i++)
+            {
+                Console.WriteLine($"Name of {i + 1} favourite color is: ");
+                string tempColor = ChekedString();
+                tempFavColors[i] = tempColor;
+            }
+            return tempFavColors;
+        }
+
+        static byte CheckedNum()
         {
             byte correctNum;
-            bool iscorrect = byte.TryParse(Console.ReadLine(), out correctNum); ;
+            bool iscorrect = byte.TryParse(Console.ReadLine(), out correctNum);
             if (iscorrect == false)
             {
                 Console.WriteLine("You enterd wrong value");
                 Console.WriteLine("Enter value again");
-                CheckedNum();
+
+                correctNum = CheckedNum();
+                return correctNum;
             }
-            return correctNum;
+            else if (correctNum > 150)
+            {
+                Console.WriteLine("You enterd wrong value");
+                Console.WriteLine("Enter value again");
+
+                correctNum = CheckedNum();
+                return correctNum;
+            }
+            else
+            {
+                return correctNum;
+            }
+        }
+
+        static string ChekedString()
+        {
+            string correctValue = "";
+            correctValue = Console.ReadLine();
+
+            int num;
+            bool iscorrect = int.TryParse(correctValue, out num);
+
+            if (iscorrect == true)
+            {
+                Console.WriteLine("You enterd wrong value");
+                Console.WriteLine("Enter value again");
+
+                correctValue = ChekedString();
+            }
+            else if (String.IsNullOrEmpty(correctValue))
+            {
+                Console.WriteLine("You enterd nothing");
+                Console.WriteLine("Enter value again");
+
+                correctValue = ChekedString();
+            }
+
+
+            return correctValue;
+
         }
 
     }
